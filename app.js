376dae7f6dc164cc878315bcf8ed76f0e953c105
DamianoMaugeri <damianomaugeri97@icloud.com>
2024-11-18 17:01:37 +0100

    [add] first step

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('hello')
+
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('server del mio blog')
+})
+
+
+
+app.listen(port, () => {
+    console.log('hello')
+})
+
+
